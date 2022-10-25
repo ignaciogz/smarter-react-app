00c280aef150103ce88b0cbb9fff2d695c3d4396
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2022-10-25 16:57:54 -0300

    [ADD] Background images into ItemList and ItemDetail component

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -9,49 +9,55 @@ const ItemDetail = ({ item }) => {
   const descriptionContent = item.description.split(". ");
 
   return (
-    <Container className="container-lg">
-      <Card className="App-item-horizontal-card">
-        <div className="row g-0 align-items-center">
-          <div className="col-md-4">
-            <button className="App-item-horizontal-back-btn">
-              <ArrowLeftCircleFill size={32}/>
-            </button>
-            <Card.Img className="img-fluid rounded-start" src={item.img} alt={`Foto de ${item.title}}`} />
-          </div>
-          <div className="col-md-8">
-            <Card.Body>
-              <div className="d-flex justify-content-between align-items-end mb-2">
-                <Card.Title>{item.title}</Card.Title>
-                <small>Stock: <span>{item.stock}</span></small>
-              </div>
-              
-              <div className="App-item-horizontal-card-price">
-                {item.price}
-                <span>Abone en 3 a 6 cuotas sin interés</span>
-              </div>
+    <section className="container-fluid" style={{ 
+			backgroundImage: `url(${process.env.PUBLIC_URL + '/img/backgrounds/gray-1800.webp'})`,
+			backgroundColor: "gray",
+    	padding: "2rem 0",
+		}}>
+      <Container className="container-lg">
+        <Card className="App-item-horizontal-card">
+          <div className="row g-0 align-items-center">
+            <div className="col-md-4">
+              <button className="App-item-horizontal-back-btn">
+                <ArrowLeftCircleFill size={32}/>
+              </button>
+              <Card.Img className="img-fluid rounded-start" src={item.img} alt={`Foto de ${item.title}}`} />
+            </div>
+            <div className="col-md-8">
+              <Card.Body>
+                <div className="d-flex justify-content-between align-items-end mb-2">
+                  <Card.Title>{item.title}</Card.Title>
+                  <small>Stock: <span>{item.stock}</span></small>
+                </div>
+                
+                <div className="App-item-horizontal-card-price">
+                  {item.price}
+                  <span>Abone en 3 a 6 cuotas sin interés</span>
+                </div>
 
-              <div className="App-item-horizontal-card-description">
-                {
-                  descriptionContent.map((element, index) => 
-                    <p key={`description-p-${index}`}>
-                      {index < descriptionContent.length - 1 ? `${element}.` : `${element}`}
-                    </p>
-                  )
-                }
-              </div>
-              
-              <div className="d-flex">
-                <ItemCount stock={Number(item.stock)}/>
-                <button className="App-item-horizontal-add-btn d-flex justify-content-center">
-                  <CartPlus size={24}/>
-                  <span>Añadir al carrito</span>
-                </button>
-              </div>
-            </Card.Body>
+                <div className="App-item-horizontal-card-description">
+                  {
+                    descriptionContent.map((element, index) => 
+                      <p key={`description-p-${index}`}>
+                        {index < descriptionContent.length - 1 ? `${element}.` : `${element}`}
+                      </p>
+                    )
+                  }
+                </div>
+                
+                <div className="d-flex">
+                  <ItemCount stock={Number(item.stock)}/>
+                  <button className="App-item-horizontal-add-btn d-flex justify-content-center">
+                    <CartPlus size={24}/>
+                    <span>Añadir al carrito</span>
+                  </button>
+                </div>
+              </Card.Body>
+            </div>
           </div>
-        </div>
-      </Card>
-    </Container>
+        </Card>
+      </Container>
+    </section>
   )
 }
 	
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -5,15 +5,21 @@ import SpinnerWidget from "../SpinnerWidget/SpinnerWidget";
 
 const ItemList = ({items}) => { 
   return (
-		<Container className="container-lg">
-			<Row className="row-gap">
-				{
-					items.length > 0
-						? items.map((item, index) => item.stock > 0 && <Item key={`item-${index}`} item={item} />)
-						: <SpinnerWidget />
-				}
-			</Row>
-		</Container>
+		<section className="container-fluid" style={{ 
+			backgroundImage: `url(${process.env.PUBLIC_URL + '/img/backgrounds/black-1800.webp'})`,
+			backgroundColor: "black",
+    	padding: "2rem 0",
+		}}>
+			<Container className="container-lg">
+				<Row className="row-gap">
+					{
+						items.length > 0
+							? items.map((item, index) => item.stock > 0 && <Item key={`item-${index}`} item={item} />)
+							: <SpinnerWidget />
+					}
+				</Row>
+			</Container>
+		</section>
 	)
 }
 
