bfa37bcd8c63f2eb3ee629dc2496661a5674edf5
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2022-10-26 15:47:36 -0300

    [RF] Loader logic added into ItemList and ItemListContainer

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,8 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Item from "../Item/Item";
 import Row from 'react-bootstrap/Row';
 import SpinnerWidget from "../SpinnerWidget/SpinnerWidget";
+import Alert from 'react-bootstrap/Alert';
 
-const ItemList = ({ items }) => {
+const ItemList = ({ items, loaded }) => {
   return (
 		<section className="App-content container-fluid" style={{ 
 			backgroundImage: `url(${process.env.PUBLIC_URL + '/img/backgrounds/black-1800.webp'})`,
@@ -11,8 +12,10 @@ const ItemList = ({ items }) => {
 			<Container className="container-lg">
 				<Row className="row-gap">
 					{
-						items.length > 0
-							? items.map((item, index) => item.stock > 0 && <Item key={`item-${index}`} item={item} />)
+						loaded 
+							? items.length > 0
+									? items.map((item, index) => item.stock > 0 && <Item key={`item-${index}`} item={item} />)
+									: <Alert variant="secondary" className="text-center fw-bold">No hay productos cargados en la API</Alert>
 							: <SpinnerWidget />
 					}
 				</Row>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -6,20 +6,23 @@ import { useParams } from 'react-router-dom';
 const ItemListContainer = () => {
     const {categoryID} = useParams();
     const [items, setItems] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
     async function fetchItems(categoryID) {
       let items = await getItems();
 
       if(categoryID) items = items.filter(element => element.categoryID === categoryID);
       setItems(items);
+      setLoaded(true);
     }
 
     useEffect(() => {
+      setLoaded(false);
       fetchItems(categoryID);
     }, [categoryID]);
  
     return (
-      <ItemList items={items} />
+      <ItemList items={items} loaded={loaded} />
     )
   }
   
