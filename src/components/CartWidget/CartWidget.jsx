02585fd68d8d56fe1729f433a26a3dff6d46f007
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2022-10-30 00:12:38 -0300

    [RF] Visibility logic added into CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,24 @@
+import { useContext } from 'react'
 import Badge from 'react-bootstrap/Badge';
 import { Cart3 } from 'react-bootstrap-icons';
 
+import CartContext from '../../context/CartContext'
 import './CartWidget.scss'
 
 const CartWidget = () => {
+  const { getItemsQty } = useContext(CartContext);
+  const qty = getItemsQty();
+
   return (
     <div className="App-cart-icon d-flex">
-      <Cart3 size={32}/>
-      <Badge bg="dark">2</Badge>
+      {qty > 0 &&
+        (
+          <>
+            <Cart3 size={32}/>
+            <Badge bg="dark">{ qty }</Badge>
+          </>
+        )
+      }
     </div>
 	)
 }
