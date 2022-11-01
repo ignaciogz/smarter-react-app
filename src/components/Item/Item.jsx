49c7f7e56345504e529db3b95742415ca16756bd
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2022-11-01 18:56:37 -0300

    [ADD] Logic of lazy loader using Intersection Observer API into Item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,33 +1,55 @@
-import { Button } from 'semantic-ui-react'
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react'
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 import { SearchHeart } from 'react-bootstrap-icons';
 import './Item.scss'
 
 const Item = ({ item }) => {
+  const elementRef = useRef();
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting }= entries[0];
+
+      console.log({ isIntersecting })
+
+      if (isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(elementRef.current);
+  }, [elementRef]);
+
   return (
-    <div className="col-6 col-sm-6 col-md-4 col-lg-3">
-      <Card className="App-item-card">
-        <Card.Img variant="top" src={item.img} alt={`Foto de ${item.title}}`} />
-        
-        <Card.Body>
-          <h2 className="card-title">{item.name}</h2>
-          <div className="d-flex justify-content-between align-items-center">  
-            <div>
-              <div className="App-item-card-price">{item.price}</div>
-              <small className="d-flex flex-column flex-lg-row">
-                <span>{item.category.name.toUpperCase()}</span>
-                <span>Stock: {item.stock}</span>
-              </small>
-            </div>
-            <Button as={Link} to={`/item/${item.id}`} className="App-item-card-details">
-              <SearchHeart size={18} />
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    </div>
+    <Col ref={elementRef} className="col-6 col-sm-6 col-md-4 col-lg-3 lazy-loader-wrapper">
+      {
+        show && <Card className="App-item-card">
+              <Card.Img variant="top" src={item.img} alt={`Foto de ${item.title}}`} />
+              
+              <Card.Body>
+                <h2 className="card-title">{item.name}</h2>
+                <div className="d-flex justify-content-between align-items-center">  
+                  <div>
+                    <div className="App-item-card-price">{item.price}</div>
+                    <small className="d-flex flex-column flex-xl-row">
+                      <span>{item.category.name.toUpperCase()}</span>
+                      <span>Stock: {item.stock}</span>
+                    </small>
+                  </div>
+                  <Button as={Link} to={`/item/${item.id}`} className="App-item-card-details">
+                    <SearchHeart size={18} />
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+      }
+    </Col>
   )
 }
 	
