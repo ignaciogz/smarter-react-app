012bdd58a05158ef9428da8596db670d09823850
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2022-11-16 18:43:23 -0300

    [RF] initializeFirestore

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import { initializeApp } from "firebase/app";
-import {initializeFirestore} from 'firebase/firestore';
 import firebaseConfig from "./services/prod_keys/firebase";
+import {initializeFirestore} from 'firebase/firestore';
 import './index.scss';
 
 const firebaseApp = initializeApp(firebaseConfig);
 initializeFirestore(firebaseApp, {
   experimentalForceLongPolling: true,
-  useFetchStreams: false,
-})
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
