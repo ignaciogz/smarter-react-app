9eec1d496b43f346d7be90d900057d8fdfbb6ae4
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2022-11-16 17:40:47 -0300

    [ADD] env variables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "./services/keys/firebase";
+import firebaseConfig from "./services/prod_keys/firebase";
 import './index.scss';
 
 initializeApp(firebaseConfig);
